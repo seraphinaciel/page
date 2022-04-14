cf65cd2705e2d72fd5f6298d00c4380e6c095ecc
seraphinaciel <99410338+seraphinaciel@users.noreply.github.com>
2022-04-15 00:10:33 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -28,10 +28,10 @@ $(document).ready(function($){
 
 //main → project load more button
 $(document).ready(function() { 
-	$(".projectList figure").slice(0, 5).show();
+	$(".projectList figure").slice(0, 4).show();
 	$(".btn_readMore").on("click", function(e) {
 		e.preventDefault();
-		$(".projectList figure:hidden").slice(0, 10).slideDown();
+		$(".projectList figure:hidden").slice(0, 4).slideDown();
 		if ($(".projectList figure:hidden").length == 0) {
 			$(".btn_readMore").text("No Content").addClass("noContent");
 		}
@@ -41,45 +41,4 @@ $(document).ready(function() {
 
 
 
-//subpage → 첫페이지, 마지막 페이지는 버튼 비활성
-const prevBtn = document.querySelector(".pageIndicator .prev");
-const nextBtn = document.querySelector(".pageIndicator .next");
-let currentLink = document.location.href; 
-const localFullName = document.URL.substring(document.URL.lastIndexOf('/') + 1);
-const localName = parseInt(localFullName.substring(localFullName.lastIndexOf('.'), 9));
 
-if(localName === 0){
-	prevBtn.disabled = true;
-}else if(localName === 13){
-	nextBtn.disabled = true;
-}
-
-function prev() {
-	if (window.location == currentLink) {
-		let pageUrl = String(localName - 1).padStart(2, "0");
-		window.location.href = 'subpage__'+pageUrl+'.html';
-		prevBtn.innerText = `hello`
-	}
-}
-function next(){
-	if (window.location == currentLink) {
-		let pageUrl = String(localName + 1).padStart(2, "0");
-		window.location.href = 'subpage__'+pageUrl+'.html';
-	}
-}
-
-function indicatorBox(){
-	if (window.location == currentLink) {
-		let pageUrl = String(localName - 1).padStart(2, "0");
-		window.location.href = 'subpage__'+pageUrl+'.html';
-	}
-}
-prevBtn.addEventListener("click", indicatorBox);
-nextBtn.addEventListener("click", indicatorBox);
-/*
-<div class="pageIndicator">
-	<button class="prev" onclick="prev()">이전</button>
-	<button class="list" onclick="location.href='../index.html'">목록</button>
-	<button class="next" onclick="next()">다음</button>
-</div>
-*/
